4defc8c20849ba95c029ee175a8152181ba5fffe
Desislava Naydenova <deskata.01@gmail.com>
2024-04-11 08:02:57 +0000

    printed the board

--- run.py
+++ run.py
@@ -7,6 +7,18 @@ print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 
 #creating the board
 
+cells = [1,2,3,4,5,6,7,8,9]
+board = [[1,2,3], [4,5,6], [7,8,9]]
+rows = 3
+columns = 3
+
+def printBoard():
+    for x in range(rows):
+        print("\n+---+---+---+")
+        print("|", end = "")
+        for y in range(columns):
+            print("", board[x][y], end=" |")
+    print("\n+---+---+---+")
 #checking for winn
 
 #player turn
@@ -18,6 +30,8 @@ print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 #checking for win
 
 #main function
+def main():
+    printBoard()
 
 #ask if the player wants to play again
 
