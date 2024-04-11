d5f8016a5e36457e756cd41296c0a888780a1ab3
Desislava Naydenova <deskata.01@gmail.com>
2024-04-11 07:50:40 +0000

    define the steps to build the game

--- run.py
+++ run.py
@@ -4,3 +4,22 @@ print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 print("Welcome to my TicTacToe Game!")
 print("          Enjoy!")
 print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+
+#creating the board
+
+#checking for winn
+
+#player turn
+
+#checking for win
+
+#computer turn
+
+#checking for win
+
+#main function
+
+#ask if the player wants to play again
+
+main()
+
